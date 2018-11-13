6e65b780766f172f438f971edf9740f1b2281bc1
buhairong <22858111@qq.com>
2018-11-13 23:02:58 +0800

    add

---
+++ NODEJS/01_node/02_module.js
@@ -0,0 +1,4 @@
+console.log("我是一个模块,我是02");
+
+exports.x="我是x";
+exports.y="我是y";
---
+++ NODEJS/01_node/03_module.js
@@ -0,0 +1,6 @@
+var md = require("./02_module");
+var math = require("./math");
+
+console.log(md);
+console.log(math.add(2,3));
+console.log(math.mul(2,3));
